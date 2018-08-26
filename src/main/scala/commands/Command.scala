a505b23bef00dacb46cac19df142c1fd2d68a274
Francisco <javiermartincuenca@gmail.com>
2018-08-26 12:31:23 +0200

    Directory structure and mkdir command created

--- src/main/scala/commands/Command.scala
+++ src/main/scala/commands/Command.scala
@@ -9,6 +9,31 @@ trait Command {
 
 object Command{
 
-  def from(input:String):Command=
-    new UnkownCommand
+  val MKDIR_NAME: String = "mkdir"
+  val CD_NAME: String = "cd"
+  val LS_NAME = "ls"
+
+  def emptyCommand: Command = new Command {
+    override def apply(state: State): State = state
+  }
+
+  def incompleteCommand(name: String): Command = new Command {
+    override def apply(state: State): State = state.setMessage(s"$name: incomplete command")
+  }
+
+  def from(input: String): Command = {
+    val tokens: Array[String] = input.split(" ")
+
+    if (input.isEmpty || tokens.isEmpty)
+      emptyCommand
+    else if (MKDIR_NAME.equals(tokens(0)))
+      if (tokens.length < 2)
+        incompleteCommand(MKDIR_NAME)
+      else
+        new Mkdir(tokens(1))
+    else if (LS_NAME.equals(tokens(0)))
+      new Ls
+    else
+      new UnkownCommand
+  }
 }
